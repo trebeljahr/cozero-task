f40e7d94995ff7b9e0f5091cc03e1757b51e5fed
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-12-03 10:08:52 +0100

    Add coloring and error handler

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,24 +18,26 @@ function MyForm(): JSX.Element {
     setInput(e.target.value)
   }
 
-  async function fetchCryptoInfo(): Promise<any> {
-    const id = input
-    console.log(id)
+  async function fetchCryptoInfo(id: string): Promise<any> {
     setLoading(true);
-    setPastSearches(Array.from(new Set([...pastSearches, id])));
     const cryptoInfo = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`, {
       headers: new Headers({ 'Content-Type': 'application/json', Accept: 'application/json' }),
     })
+    setLoading(false);
     return cryptoInfo
   }
 
   async function submit(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault()
-    const response = await fetchCryptoInfo()
+    const response = await fetchCryptoInfo(input)
     const cryptoInfo = await response.json()
-    console.log(cryptoInfo)
+
+    if ("error" in cryptoInfo) {
+      alert("There was an error fetching the id - try using lower case");
+      return 
+    }
+    setPastSearches(Array.from(new Set([...pastSearches, input])));
     setResult(cryptoInfo)
-    setLoading(false);
   }
 
   return (
@@ -59,14 +61,15 @@ function MyForm(): JSX.Element {
   )
 }
 
-function Presentation({crypto}:any):any{
+function Presentation({crypto}:any):JSX.Element{
+  const isBadCrypto = crypto.market_data.price_change_percentage_24h <= 0
   return <div id="crypto-presentation">
     <img src={crypto.image.thumb} alt={`${crypto.name} logo`}/> 
     <h1>{crypto.name}</h1>
-    <h2>Market Cap Rank: {crypto.market_cap_rank} </h2>
-    <h3>Symbol: {crypto.symbol} </h3>
-    <p>Percentual 24h price change: {crypto.market_data.price_change_percentage_24h}</p>
-    <p>Current price in USD: {crypto.market_data.current_price.usd} </p>
+    <h2>Market Cap Rank: {crypto.market_cap_rank}</h2>
+    <h3>Symbol: {crypto.symbol}</h3>
+    <p style={{color: isBadCrypto ? "red": "green"}}>Percentual 24h price change: {crypto.market_data.price_change_percentage_24h}</p>
+    <p>Current price in USD: {crypto.market_data.current_price.usd}</p>
   </div>
 }
 
